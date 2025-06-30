a1727c96b28fbb28762b7827c54a78120a099ba7
mcatherine1994 <mcatherine1994@gmail.com>
2025-06-30 11:58:44 -0700

    fix(api): #791 fix date string type check

--- api/src/app/modules/analytics-dashboard/analytics-dashboard.dto.ts
+++ api/src/app/modules/analytics-dashboard/analytics-dashboard.dto.ts
@@ -1,12 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDateString } from 'class-validator';
+import { IsISODateOnlyString } from '@api-modules/interaction/interaction.dto';
+import { DateTimeUtil } from '@api-core/dateTimeUtil';
 
 export class DateRangeRequest {
   @ApiProperty({ description: 'Start date in YYYY-MM-DD format' })
-  @IsDateString()
+  @IsISODateOnlyString({message: `"$property" must be ISO-formatted date. (Required format: ${DateTimeUtil.DATE_FORMAT})`})
   startDate: string;
 
   @ApiProperty({ description: 'End date in YYYY-MM-DD format' })
-  @IsDateString()
+  @IsISODateOnlyString({message: `"$property" must be ISO-formatted date. (Required format: ${DateTimeUtil.DATE_FORMAT})`})
   endDate: string;
 }
