fed548122607bb7004b211fa889d1cac8432cc08
Derek Roberts <derek.roberts@gmail.com>
2026-03-10 13:17:48 -0700

    Pagination status toggle

--- admin/src/app/foms/review-comments/comment-detail/comment-detail.component.ts
+++ admin/src/app/foms/review-comments/comment-detail/comment-detail.component.ts
@@ -25,6 +25,66 @@ import { DatePipe, NgFor, NgIf } from '@angular/common';
   exportAs: 'commentForm'
 })
 export class CommentDetailComponent {
+    @Input() comments: PublicCommentAdminResponse[] = [];
+    @Input() currentCommentIndex: number = 0;
+
+    // Pagination controls
+    selectPrevComment() {
+      if (this.currentCommentIndex > 0) {
+        this.currentCommentIndex--;
+        this.emitCommentChange();
+      }
+    }
+
+    selectNextComment() {
+      if (this.currentCommentIndex < this.comments.length - 1) {
+        this.currentCommentIndex++;
+        this.emitCommentChange();
+      }
+    }
+
+    selectNextUnactioned() {
+      for (let i = this.currentCommentIndex + 1; i < this.comments.length; i++) {
+        if (!this.comments[i].response || !this.comments[i].response.code) {
+          this.currentCommentIndex = i;
+          this.emitCommentChange();
+          return;
+        }
+      }
+    }
+
+    isFirstComment(): boolean {
+      return this.currentCommentIndex === 0;
+    }
+
+    isLastComment(): boolean {
+      return this.currentCommentIndex === this.comments.length - 1;
+    }
+
+    isLastUnactioned(): boolean {
+      for (let i = this.currentCommentIndex + 1; i < this.comments.length; i++) {
+        if (!this.comments[i].response || !this.comments[i].response.code) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    getCurrentCommentIndex(): number {
+      return this.currentCommentIndex;
+    }
+
+    getTotalComments(): number {
+      return this.comments.length;
+    }
+
+    emitCommentChange() {
+      // TODO: Emit event to parent to update selected comment
+    }
+
+    setResponseCode(code: string) {
+      this.commentFormGroup.get('responseCode').setValue(code);
+    }
   commentScopeCodes = indexBy(this.stateSvc.getCodeTable('commentScopeCode'), (x) => x.code);
   commentFormGroup: IFormGroup<CommentDetailForm>;
   comment: PublicCommentAdminResponse;
