322721a59a43c242b37ba03a45f53273175cf05b
Derek Roberts <derek.roberts@gmail.com>
2026-03-26 20:29:26 -0700

    test: add tslib moduleNameMapper to fix cross-directory lib imports

--- admin/jest.config.js
+++ admin/jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
     '^@api-client$': '<rootDir>/../libs/client/typescript-ng',
     '^@utility/(.*)$': '<rootDir>/../libs/utility/src/$1',
     '^app/(.*)$': '<rootDir>/src/app/$1',
+    '^tslib$': '<rootDir>/node_modules/tslib',
   },
   coverageDirectory: '../coverage/admin',
   snapshotSerializers: [
