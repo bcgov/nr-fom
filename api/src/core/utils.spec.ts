605684a4d8778fa63ca21b279d1048a59cb47d0d
Derek Roberts <derek.roberts@gmail.com>
2026-04-01 10:31:57 -0700

    test(api): fix flatDeep tests to match actual function behavior

--- api/src/core/utils.spec.ts
+++ api/src/core/utils.spec.ts
@@ -1,17 +1,22 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { flatDeep } from './utils';
 
 describe('flatDeep', () => {
-  it('should flatten nested arrays', () => {
+  it('should flatten nested arrays with sufficient depth', () => {
     const arr = [ 1, [ 2, [ 3, [ 4 ] ], 5 ] ];
     expect(flatDeep(arr, 10)).toEqual([ 1, 2, 3, 4, 5 ]);
+  });
+
+  it('should flatten one level by default', () => {
+    const arr = [ 1, [ 2, 3 ], [ 4, [ 5 ] ] ];
+    expect(flatDeep(arr)).toEqual([ 1, 2, 3, 4, [ 5 ] ]);
   });
 
   it('should return empty array if input is empty', () => {
     expect(flatDeep([])).toEqual([]);
   });
 
-  it('should handle non-array input', () => {
-    expect(flatDeep(1)).toEqual([ 1 ]);
+  it('should return shallow copy when depth is 0', () => {
+    const arr = [ 1, [ 2, [ 3 ] ] ];
+    expect(flatDeep(arr, 0)).toEqual([ 1, [ 2, [ 3 ] ] ]);
   });
 });
