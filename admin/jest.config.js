5f1e73ce258219e87e92527fb23f24d03aaa1fa3
Derek Roberts <derek.roberts@gmail.com>
2026-04-06 17:55:03 -0700

    fix(ci): remove symlinks, add roots to jest configs to fix module resolution

--- admin/jest.config.js
+++ admin/jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   testMatch: [
     '<rootDir>/src/**/*.spec.ts',
   ],
+  roots: ['<rootDir>/src'],
   moduleNameMapper: {
     '^@admin-core/(.*)$': '<rootDir>/src/core/$1',
     '^@api-client$': '<rootDir>/../libs/client/typescript-ng',
