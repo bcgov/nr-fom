75d3e563d82aea8505ddb726c31c45ebb14aa02f
Ian Liu <ianliuwk1019@gmail.com>
2026-06-01 12:24:35 -0700

    Revert unnecessary change from Angular 21 upgrade.

--- public/src/app/header/header.component.ts
+++ public/src/app/header/header.component.ts
@@ -1,9 +1,7 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Router, RouterLink, NavigationEnd } from '@angular/router';
-import { distinctUntilChanged, filter, map } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { ConfigService } from '@utility/services/config.service';
 
 @Component({
@@ -21,26 +19,12 @@ import { ConfigService } from '@utility/services/config.service';
     ])
   ]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   environmentDisplay: string;
   isNavMenuOpen = false; 
-  currentUrl = '';
-  private destroyRef = inject(DestroyRef);
 
   constructor(private configService: ConfigService, public router: Router) {
     this.environmentDisplay = this.configService.getEnvironmentDisplay();
-    this.currentUrl = this.router.url;
-  }
-
-  ngOnInit(): void {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-      map(() => this.router.url),
-      distinctUntilChanged(),
-      takeUntilDestroyed(this.destroyRef)
-    ).subscribe((url) => {
-      this.currentUrl = url;
-    });
   }
 
   toggleNav() {
