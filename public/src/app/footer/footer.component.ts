a9584478f727d8a4235654350052fb0bb781c659
Derek Roberts <derek.roberts@gmail.com>
2026-07-03 14:25:07 -0700

    fix(footer): refactor routing check logic to use router NavigationEnd events for isProjectsPage state

--- public/src/app/footer/footer.component.ts
+++ public/src/app/footer/footer.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, RouterLink, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   standalone: true,
@@ -9,6 +11,24 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, OnDestroy {
+  public isProjectsPage = false;
+  private routerSubscription: Subscription;
+
   constructor(public router: Router) {}
+
+  ngOnInit(): void {
+    this.isProjectsPage = this.router.url.includes('projects');
+    this.routerSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isProjectsPage = event.urlAfterRedirects.includes('projects');
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
 }
