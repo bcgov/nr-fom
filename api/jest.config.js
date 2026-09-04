e8ab61dc8ab35825a3d19103018ef1097ed941ea
Derek Roberts <derek.roberts@gmail.com>
2026-09-04 14:12:35 -0700

    fix(deps): bump fast-uri, qs, and decode-uri-component to resolve security alerts

--- api/jest.config.js
+++ api/jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   "testRegex": ".*\\.(spec|e2e-spec)\\.ts$",
   "modulePaths": ["<rootDir>/src", "<rootDir>/src/"],
-  transformIgnorePatterns: ['node_modules/(?!(jose|jwks-rsa)/)'],
+  transformIgnorePatterns: ['node_modules/(?!(jose|jwks-rsa|decode-uri-component)/)'],
   // 'moduleNameMapper' important to map some 'paths' from tsconfig.json for jest.
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' } ),
   moduleFileExtensions: ['ts', 'js'],
